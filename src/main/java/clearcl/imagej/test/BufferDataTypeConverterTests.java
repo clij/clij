1bc92079d761589fca3874daff6ed0f1f0a72268
haesleinhuepf <rhaase@mpi-cbg.de>
2018-04-15 08:48:25 +0200

    added simple execution tests for converting between types

---
+++ src/main/java/clearcl/imagej/test/BufferDataTypeConverterTests.java
@@ -0,0 +1,43 @@
+package clearcl.imagej.test;
+
+import clearcl.ClearCLBuffer;
+import clearcl.imagej.ClearCLIJ;
+import clearcl.imagej.kernels.Kernels;
+import coremem.enums.NativeTypeEnum;
+import org.junit.Test;
+
+/**
+ * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
+ * April 2018
+ */
+public class BufferDataTypeConverterTests
+{
+  @Test
+  public void testFloatToByteConversion() {
+    ClearCLIJ clij = ClearCLIJ.getInstance();
+    ClearCLBuffer bufferIn = clij.createCLBuffer(new long[]{10,10,10}, NativeTypeEnum.Float);
+    ClearCLBuffer bufferOut = clij.createCLBuffer(new long[]{10,10,10}, NativeTypeEnum.Byte);
+
+    Kernels.copy(clij, bufferIn, bufferOut);
+  }
+
+
+  @Test
+  public void testFloatToUnsignedShortConversion() {
+    ClearCLIJ clij = ClearCLIJ.getInstance();
+    ClearCLBuffer bufferIn = clij.createCLBuffer(new long[]{10,10,10}, NativeTypeEnum.Float);
+    ClearCLBuffer bufferOut = clij.createCLBuffer(new long[]{10,10,10}, NativeTypeEnum.UnsignedShort);
+
+    Kernels.copy(clij, bufferIn, bufferOut);
+  }
+
+
+  @Test
+  public void testUnsignedShortToByteConversion() {
+    ClearCLIJ clij = ClearCLIJ.getInstance();
+    ClearCLBuffer bufferIn = clij.createCLBuffer(new long[]{10,10,10}, NativeTypeEnum.UnsignedShort);
+    ClearCLBuffer bufferOut = clij.createCLBuffer(new long[]{10,10,10}, NativeTypeEnum.Byte);
+
+    Kernels.copy(clij, bufferIn, bufferOut);
+  }
+}
