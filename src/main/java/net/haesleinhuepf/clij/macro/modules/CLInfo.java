8550d662330b1236378498465910828c58fcbbd1
haesleinhuepf <rhaase@mpi-cbg.de>
2019-01-01 14:13:21 +0100

    add plugin for CLInfo

---
+++ src/main/java/net/haesleinhuepf/clij/macro/modules/CLInfo.java
@@ -0,0 +1,40 @@
+package net.haesleinhuepf.clij.macro.modules;
+
+import ij.IJ;
+import net.haesleinhuepf.clij.CLIJ;
+import net.haesleinhuepf.clij.macro.AbstractCLIJPlugin;
+import net.haesleinhuepf.clij.macro.CLIJHandler;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
+import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Author: @haesleinhuepf
+ * 12 2018
+ */
+
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJ_clInfo")
+public class CLInfo extends AbstractCLIJPlugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+
+    @Override
+    public boolean executeCL() {
+        IJ.log(CLIJ.clinfo());
+        return true;
+    }
+
+    @Override
+    public String getParameterHelpText() {
+        return "";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Outputs information about available OpenCL devices.";
+    }
+
+    @Override
+    public String getAvailableForDimensions() {
+        return "";
+    }
+}
