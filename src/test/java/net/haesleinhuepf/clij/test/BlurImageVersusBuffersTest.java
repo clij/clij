8fd9f0e085427e7c9ef58e2cab227748f809af29
haesleinhuepf <rhaase@mpi-cbg.de>
2019-01-04 17:02:47 +0100

    added a test to compare blur on buffers and images

---
+++ src/test/java/net/haesleinhuepf/clij/test/BlurImageVersusBuffersTest.java
@@ -0,0 +1,42 @@
+package net.haesleinhuepf.clij.test;
+
+import clearcl.ClearCLBuffer;
+import clearcl.ClearCLImage;
+import clearcl.util.ElapsedTime;
+import ij.ImagePlus;
+import net.haesleinhuepf.clij.CLIJ;
+import net.haesleinhuepf.clij.kernels.Kernels;
+import org.junit.Test;
+
+import static net.haesleinhuepf.clij.utilities.CLIJUtilities.sigmaToKernelSize;
+
+public class BlurImageVersusBuffersTest {
+    @Test
+    public void compareImagesAndBufferProcessingTime(){
+        CLIJ clij = CLIJ.getInstance();
+        ImagePlus testImage = TestUtilities.getRandomImage(100, 100, 100, 32, 0, 100);
+
+        ClearCLBuffer inputBuffer = clij.convert(testImage, ClearCLBuffer.class);
+        ClearCLBuffer outputBuffer = clij.createCLBuffer(inputBuffer);
+
+        ClearCLImage inputImage = clij.convert(testImage, ClearCLImage.class);
+        ClearCLImage outputImage = clij.createCLImage(inputImage);
+
+        float sigma = 5;
+
+        for (int i = 0; i < 10; i++) {
+            ElapsedTime.measureForceOutput("blur buffer", () -> {
+                Kernels.blur(clij, inputBuffer, outputBuffer, sigmaToKernelSize(sigma), sigmaToKernelSize(sigma), sigmaToKernelSize(sigma), sigma, sigma, sigma);
+            });
+            ElapsedTime.measureForceOutput("blur image", () -> {
+                Kernels.blur(clij, inputImage, outputImage, sigmaToKernelSize(sigma), sigmaToKernelSize(sigma), sigmaToKernelSize(sigma), sigma, sigma, sigma);
+            });
+            ElapsedTime.measureForceOutput("blurSep buffer", () -> {
+                Kernels.blurFast(clij, inputBuffer, outputBuffer, sigma, sigma, sigma);
+            });
+            ElapsedTime.measureForceOutput("blurSep image", () -> {
+                Kernels.blurFast(clij, inputImage, outputImage, sigma, sigma, sigma);
+            });
+        }
+    }
+}
