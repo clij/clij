4a12a108980ac4aad7a7b66cd63a794c6a228001
haesleinhuepf <rhaase@mpi-cbg.de>
2019-01-07 23:21:35 +0100

    rewrite ImagePlus->CLBuffer conversion

--- src/main/java/net/haesleinhuepf/clij/converters/implementations/ImagePlusToClearCLBufferConverter.java
+++ src/main/java/net/haesleinhuepf/clij/converters/implementations/ImagePlusToClearCLBufferConverter.java
@@ -1,12 +1,18 @@
 package net.haesleinhuepf.clij.converters.implementations;
 
 import clearcl.ClearCLBuffer;
+import coremem.ContiguousMemoryInterface;
+import coremem.enums.NativeTypeEnum;
 import ij.ImagePlus;
 import net.haesleinhuepf.clij.converters.AbstractCLIJConverter;
 import net.haesleinhuepf.clij.converters.CLIJConverterPlugin;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import org.scijava.plugin.Plugin;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 /**
  * ImagePlusToRandomAccessibleIntervalConverter
@@ -21,6 +27,59 @@ public class ImagePlusToClearCLBufferConverter extends AbstractCLIJConverter<Ima
 
     @Override
     public ClearCLBuffer convert(ImagePlus source) {
+
+
+        long[] dimensions = new long[source.getNSlices() == 1?2:3];
+        dimensions[0] = source.getWidth();
+        dimensions[1] = source.getHeight();
+        if (source.getNSlices() > 1) {
+            dimensions[2] = source.getNSlices();
+        }
+
+        int numberOfPixelsPerSlice = (int)(dimensions[0] * dimensions[1]);
+        long numberOfPixels = dimensions[0] * dimensions[1] * dimensions[2];
+
+        NativeTypeEnum type;
+        if (source.getBitDepth() == 8) {
+            ClearCLBuffer target = clij.createCLBuffer(dimensions, NativeTypeEnum.UnsignedByte);
+
+            byte[] inputArray = new byte[(int) numberOfPixels];
+            for (int z = 0; z < target.getDepth(); z++) {
+                source.setSlice(z + 1);
+                byte[] sourceArray = (byte[])(source.getProcessor().getPixels());
+                System.arraycopy(sourceArray, 0, inputArray, z * numberOfPixelsPerSlice, sourceArray.length);
+            }
+            ByteBuffer byteBuffer = ByteBuffer.wrap(inputArray);
+            target.readFrom(byteBuffer, true);
+
+
+        } else if (source.getBitDepth() == 16) {
+            ClearCLBuffer target = clij.createCLBuffer(dimensions, NativeTypeEnum.UnsignedShort);
+
+            short[] inputArray = new short[(int) numberOfPixels];
+            for (int z = 0; z < target.getDepth(); z++) {
+                source.setSlice(z + 1);
+                short[] sourceArray = (short[])(source.getProcessor().getPixels());
+                System.arraycopy(sourceArray, 0, inputArray, z * numberOfPixelsPerSlice, sourceArray.length);
+            }
+            ShortBuffer byteBuffer = ShortBuffer.wrap(inputArray);
+            target.readFrom(byteBuffer, true);
+        } else{
+            ClearCLBuffer target = clij.createCLBuffer(dimensions, NativeTypeEnum.Float);
+
+            float[] inputArray = new float[(int) numberOfPixels];
+            for (int z = 0; z < target.getDepth(); z++) {
+                source.setSlice(z + 1);
+                float[] sourceArray = (float[])(source.getProcessor().getPixels());
+                System.arraycopy(sourceArray, 0, inputArray, z * numberOfPixelsPerSlice, sourceArray.length);
+            }
+            FloatBuffer byteBuffer = FloatBuffer.wrap(inputArray);
+            target.readFrom(byteBuffer, true);
+        }
+        return null;
+    }
+
+    public ClearCLBuffer convertLegacy(ImagePlus source) {
         RandomAccessibleInterval rai = ImageJFunctions.wrapReal(source);
         RandomAccessibleIntervalToClearCLBufferConverter raitcclbc = new RandomAccessibleIntervalToClearCLBufferConverter();
         raitcclbc.setCLIJ(clij);
