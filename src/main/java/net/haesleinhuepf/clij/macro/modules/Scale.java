15716212cc5948b5052b134eb844da4d6ff989c2
haesleinhuepf <rhaase@mpi-cbg.de>
2019-01-24 21:36:32 +0100

    added scaling with interpolation

---
+++ src/main/java/net/haesleinhuepf/clij/macro/modules/Scale.java
@@ -0,0 +1,68 @@
+package net.haesleinhuepf.clij.macro.modules;
+
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.clearcl.ClearCLImage;
+import net.haesleinhuepf.clij.kernels.Kernels;
+import net.haesleinhuepf.clij.macro.AbstractCLIJPlugin;
+import net.haesleinhuepf.clij.macro.CLIJHandler;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
+import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij.utilities.AffineTransform;
+import net.imglib2.realtransform.AffineTransform3D;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Author: @haesleinhuepf
+ * 12 2018
+ */
+
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJ_scale")
+public class Scale extends AbstractCLIJPlugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+
+    @Override
+    public boolean executeCL() {
+        float scaleFactor = asFloat(args[2]);
+        boolean rotateAroundCenter = asBoolean(args[3]);
+
+        AffineTransform3D at = new AffineTransform3D();
+        Object[] args = openCLBufferArgs();
+
+        if (rotateAroundCenter) {
+            ClearCLBuffer input = (ClearCLBuffer) args[0];
+            at.translate(-input.getWidth() / 2, -input.getHeight() / 2, -input.getDepth() / 2);
+        }
+
+        at.scale(scaleFactor);
+
+        if (rotateAroundCenter) {
+            ClearCLBuffer input = (ClearCLBuffer) args[0];
+            at.translate(input.getWidth() / 2, input.getHeight() / 2, input.getDepth() / 2);
+        }
+
+        ClearCLImage input = CLIJHandler.getInstance().getChachedImageByBuffer((ClearCLBuffer) args[0]);
+        ClearCLImage output = CLIJHandler.getInstance().getChachedImageByBuffer((ClearCLBuffer) args[1]);
+
+        boolean result = Kernels.affineTransform(clij, input, output, AffineTransform.matrixToFloatArray(at));
+
+        Kernels.copy(clij, output, (ClearCLBuffer)args[1]);
+
+        return result;
+
+    }
+
+    @Override
+    public String getParameterHelpText() {
+        return "Image source, Image destination, Number scaling_factor, Boolean scale_to_center";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Scales an image with a given factor.";
+    }
+
+    @Override
+    public String getAvailableForDimensions() {
+        return "2D, 3D";
+    }
+}
